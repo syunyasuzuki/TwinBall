4d1b9a6f8f5705160eb97882ded565f95b916eec
UmemuraYuto <polo0820yuto@yahoo.co.jp>
2019-06-24 14:52:54 +0900

    Playercon作成

---
+++ TwinBall/Assets/Script/PlayerCon2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon2 : MonoBehaviour {
+
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+    }
+}
