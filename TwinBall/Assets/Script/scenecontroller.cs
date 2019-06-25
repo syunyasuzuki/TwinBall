f4ba5ecfa8699410c1a754dd255480a2087e257c
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2019-06-25 13:58:38 +0900

    フェードイン、アウト追加

---
+++ TwinBall/Assets/Script/scenecontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenecontroller : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (SceneManager.GetActiveScene().name == "TitleScene")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                FadeController.isFade = true;
+                FadeController.isFadeOut = true;
+                Invoke("gamescene", 1.5f);
+            }
+        }
+	}
+
+    void gamescene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
